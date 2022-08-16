bb9a6b646d5d29dc04ef74b30cbb1b6220ddb90b
Carlos Pieper <capieper.pieper@gmail.com>
2022-08-16 20:58:52 -0300

    test

--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,11 +1,31 @@
+using CloudWeather.Precipitation.DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<PrecipitationDbContext>(options =>
+{
+    options.EnableSensitiveDataLogging();
+    options.EnableDetailedErrors();
+    options.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+}, ServiceLifetime.Transient
+);
+
 var app = builder.Build();
 
-app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days) =>
+app.MapGet("/observation/{zip}", async (string zip, [FromQuery] int? days, PrecipitationDbContext db) =>
 {
-    return Results.Ok(zip);
+    if (days == null || days < 1 || days > 30)
+    {
+        return Results.BadRequest("Provide days query between 1 and 30");
+    }
+    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = await db.Precipitation
+    .Where(x => x.ZipCode == zip && x.CreatedOn > startDate)
+    .ToListAsync();
+
+    return Results.Ok(results);
 });
 
 app.Run();
